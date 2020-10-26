7b7454f67be0db207c24e83fb0ad8c6e71142ac0
Renzo Vargas <teztortsg@gmail.com>
2020-10-26 12:49:35 -0500

    Creacion de todas las armas

---
+++ Assets/Scripts/Weapons/FireArms/PistolShooter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistolShooter : FireArm
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        bulletSpawnPosition = new Vector3(bulletSpawnPosition.x + 0.5f, bulletSpawnPosition.y + 0.15f, bulletSpawnPosition.z);
+        
+        //Default pistol values
+        if (fireRange == 0.0f) fireRange = 8.0f;
+        if (fireRate == 0.0f) fireRate = 0.8f;
+    }
+    
+    protected override void Spray()
+    {
+        GameObject bulletInstance = Instantiate(bulletObject, bulletSpawnPosition, Quaternion.identity);
+        bulletInstance.GetComponent<BulletMovement>().setTravelDistance(fireRange);
+    }
+
+}
---
+++ Assets/Scripts/Weapons/FireArms/ToolController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject toolQ;
+    [SerializeField]
+    private GameObject toolW;
+    [SerializeField]
+    private GameObject toolE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject toolQinstance = Instantiate(toolQ, transform.position, Quaternion.identity);
+        GameObject toolWinstance = Instantiate(toolW, transform.position, Quaternion.identity);
+        toolWinstance.SetActive(false);
+        GameObject toolEinstance = Instantiate(toolE, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (Input.GetKeyDown("q"))
+        {
+            ChangeWeapon(weaponQ)
+        }
+        if (Input.GetKeyDown("w"))
+        {
+            ChangeWeapon()
+        }
+        if (Input.GetKeyDown("e"))
+        {
+            ChangeWeapon()
+        }*/
+    }
+
+    void ChangeWeapon(string weapon) { 
+        
+    }
+}
