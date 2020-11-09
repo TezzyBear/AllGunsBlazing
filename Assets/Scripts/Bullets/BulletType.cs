2b5272f94226ba1af0e480494d83c8fdb11f002f
Renzo Vargas <teztortsg@gmail.com>
2020-11-08 20:20:38 -0500

    Holaaaaaa

---
+++ Assets/Scripts/Bullets/BulletType.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletType : MonoBehaviour
+{    public enum Type {
+        NONE,
+        PIERCING,
+        BLEEDING,
+        EXPLOSIVE,
+        BLUNDERING,
+        INCENDIARY,
+        FREEZING
+    }
+
+    public Type type;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
