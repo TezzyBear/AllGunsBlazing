713f670e38ce7d812ac2f453d7c828aae0b239fa
Juan Diego <u201213280@upc.edu.pe>
2020-11-08 21:43:50 -0500

    barra de vidas y escudos de los enemigos

---
+++ Assets/Scripts/UI/EnemyHealthBarController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthBarController : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public int maxShield = 100;
+    public int currentShield;
+    public HealthBar healthBar;
+    public ShieldBar shieldBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+        currentShield = maxShield;
+        shieldBar.SetMaxShield(maxShield);
+        shieldBar.SetColor(0.0f, 0.0f, 255.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamageHealth(20);
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadMinus))
+        {
+            TakeDamageShield(20);
+        }
+
+    }
+
+    void TakeDamageHealth(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+    void TakeDamageShield(int damage)
+    {
+        currentShield -= damage;
+        shieldBar.SetShield(currentShield);
+    }
+}
---
+++ Assets/Scripts/UI/ShieldBar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShieldBar : MonoBehaviour
+{
+    public Slider slider;
+    public Image fill;
+
+    public void SetMaxShield(int shield)
+    {
+        slider.maxValue = shield;
+        slider.value = shield;
+    }
+
+    public void SetShield(int shield)
+    {
+        slider.value = shield;
+    }
+
+    public void SetColor(float r, float g, float b)
+    {
+        fill.color = new Color(r, g, b);
+    }
+}
